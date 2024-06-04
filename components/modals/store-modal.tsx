5812fd270c5b46fdd55fcb29e144bcc840eb1233
Carla Cristine <carlacs.cristine@gmail.com>
2024-06-04 19:23:38 -0300

    form

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -5,7 +5,14 @@ import { zodResolver } from "@hookform/resolvers/zod";
 
 import { useStoreModal } from "@/hooks/use-store-modal";
 import { Modal } from "@/components/ui/modal";
-import { Form, FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 
@@ -39,20 +46,23 @@ export const StoreModal = () => {
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)}>
               <FormField
-              control={form.control}
-              name="name"
-              render={({ field }) => (
-                 <FormItem>
-                  <FormLabel>Name</FormLabel>
-                  <FormControl>
-                    <Input placeholder="E-commerce" {...field} />
-                  </FormControl>
-                 </FormItem>
-              )}
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="E-commerce" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
               />
               <div className="pt-6 space-x-2 flex items-center justify-end w-full">
-                  <Button>Cancel</Button>
-                  <Button>Continue</Button>
+                <Button variant="outline" onClick={storeModal.onClose}>
+                  Cancel
+                </Button>
+                <Button type="submit">Continue</Button>
               </div>
             </form>
           </Form>
